12b4bfab1d5c0b8f05d96672ae8678635b402338
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2021-03-23 17:40:01 +0700

    add sharePreferences

--- MyNote/app/src/main/java/com/example/mynote/EditNoteActivity.kt
+++ MyNote/app/src/main/java/com/example/mynote/EditNoteActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
 import  com.example.mynote.MyApplication.Companion.notes
+import com.google.gson.Gson
+import java.util.*
 
 class EditNoteActivity : AppCompatActivity() {
     private var titleView:EditText? = null
@@ -21,10 +23,19 @@ class EditNoteActivity : AppCompatActivity() {
         tagView = findViewById(R.id.tagData)
         noteId = intent.getIntExtra("noteId",-1)
         if (noteId != -1){
-            titleView!!.setText(notes!![noteId].title)
-            dateView!!.setText(notes!![noteId].getDate())
-            dataView!!.setText(notes!![noteId].text)
-            tagView!!.setText(notes!![noteId].tag)
+            titleView!!.setText(notes[noteId].title)
+            dateView!!.setText(notes[noteId].getDate())
+            dataView!!.setText(notes[noteId].text)
+            tagView!!.setText(notes[noteId].tag)
+        }
+        else{
+            notes.add(Note("", Date()))
+            noteId = notes.size - 1
+            dateView!!.setText(notes[noteId].getDate())
+            val sharedPreferences = applicationContext.getSharedPreferences("notes", MODE_PRIVATE)
+            val gson = Gson()
+            val json = gson.toJson(notes)
+            sharedPreferences.edit().putString("notes", json).apply()
         }
     }
 }
--- MyNote/app/src/main/java/com/example/mynote/MainActivity.kt
+++ MyNote/app/src/main/java/com/example/mynote/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.mynote
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
 import android.widget.AdapterView
 import android.widget.GridView
 import android.widget.ListView
-import java.util.*
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mynote.MyApplication.Companion.notes
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import timber.log.Timber
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     var adapter:NoteAdapter? = null
@@ -16,23 +22,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val listView = findViewById<ListView>(R.id.listView)
         val gridView = findViewById<GridView>(R.id.grid_view)
-        adapter = NoteAdapter(this, notes!!)
-        adapter!!.add(Note("abc", Date()))
-        adapter!!.add(Note("abcd", Date()))
-        adapter!!.add(Note("abce", Date()))
+        val sharedPreferences = applicationContext.getSharedPreferences("notes", MODE_PRIVATE)
+        val gson = Gson()
+        val json: String? = sharedPreferences.getString("notes", null)
+        json?.let {
+            val type = object : TypeToken<ArrayList<Note?>?>() {}.type
+            notes = gson.fromJson(json, type)
+        }
+        adapter = NoteAdapter(this, notes)
         listView.adapter = adapter
         gridView.adapter = adapter
-        listView.setOnItemClickListener(AdapterView.OnItemClickListener() { adapterView, view, i, l->
-            val intent = Intent(applicationContext,EditNoteActivity::class.java)
-            intent.putExtra("noteId",i)
+        listView.setOnItemClickListener(AdapterView.OnItemClickListener() { adapterView, view, i, l ->
+            val intent = Intent(applicationContext, EditNoteActivity::class.java)
+            intent.putExtra("noteId", i)
             startActivity(intent)
         }
         )
-        gridView.setOnItemClickListener(AdapterView.OnItemClickListener() { adapterView, view, i, l->
-            val intent = Intent(applicationContext,EditNoteActivity::class.java)
-            intent.putExtra("noteId",i)
+        gridView.setOnItemClickListener(AdapterView.OnItemClickListener() { adapterView, view, i, l ->
+            val intent = Intent(applicationContext, EditNoteActivity::class.java)
+            intent.putExtra("noteId", i)
             startActivity(intent)
         }
         )
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        val sharedPreferences = applicationContext.getSharedPreferences("notes", MODE_PRIVATE)
+        val gson = Gson()
+        val json = gson.toJson(notes)
+        sharedPreferences.edit().putString("notes", json).apply()
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    fun onAddNewNote(view: View){
+        val intent = Intent(this, EditNoteActivity::class.java)
+        startActivity(intent)
     }
 }
